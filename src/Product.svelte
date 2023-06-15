326073ee4c79a6263bb4c3fefd326f399f7c83fb
Samir Prajapati <38998432+bullseye405@users.noreply.github.com>
2023-06-15 10:33:05 +0545

    first commit

---
+++ src/Product.svelte
@@ -0,0 +1,29 @@
+<div>
+  <h1>Title</h1>
+  <h2>Price</h2>
+  <h3>Description</h3>
+
+  <button>Button</button>
+</div>
+
+<style>
+  div {
+    margin: 1rem;
+    background-color: white;
+  }
+
+  h1 {
+    font-size: 1.25rem;
+    margin: 0.25rem 0;
+  }
+
+  h2 {
+    font-size: 1rem;
+  }
+
+  button {
+    padding: 0.5rem;
+    background-color: rgb(178, 32, 32);
+    color: blue;
+  }
+</style>
